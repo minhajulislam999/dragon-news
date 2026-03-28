3b8ffccd8969d7c5c70a43d1a680b1dca69b09e8
minhajul islam <mm01882390860@gmail.com>
2026-03-28 20:59:35 +0600

    dragon file setup done

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState,  } from "react";
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
