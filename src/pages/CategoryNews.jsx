f47e036f997679e918da61b7e53c377557d09625
minhajul islam <mm01882390860@gmail.com>
2026-03-30 09:38:13 +0600

    category news added

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const CategoryNews = () => {
+  const [category, setCategory] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+    .then((res) => res.json())
+    .then((data) =>{
+      console.log(data.data);
+      setCategory(data.data);
+    })
+  }, [id]);
+  
+  console.log("Current Category ID:", id);
+
+
   return (
     <div>
-      categoryNews
+      
+  <div className="max-w-4xl mx-auto my-8">
+    <h2 className="text-2xl font-bold mb-4">Category: {id}</h2>
+    <div className="flex flex-col gap-4">
+      {
+        category.length === 0 ? 
+  <p className="text-center text-gray-500 text-xl mt-10">এই category-তে কোনো news নেই!</p>
+  :
+        category.map((news) => (
+          <div key={news._id} className="flex gap-4 bg-white p-4 rounded-lg shadow-md">
+            <img src={news.thumbnail_url} alt={news.title} className="w-48 h-32 object-cover rounded" />
+            <div>
+              <h3 className="text-xl font-semibold">{news.title}</h3>
+              <p className="text-gray-500">✍️ {news.author.name}</p>
+              <p className="text-gray-500">👁️ {news.total_view} views</p>
+              <p className="text-yellow-500">⭐ {news.rating.number}</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  </div>
+)
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const [category, setCategory] = useState([])
+
+  useEffect(()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+      .then(res => res.json())
+      .then(data =>{
+        console.log(data);
+
+        setCategory(data.data.news_category)
+        
+      } )
+  }, [])
+
   return (
     <div>
-      thins home
+         <h1 className='text-3xl font-bold text-center my-5'>All Category</h1>
+
+         
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        {
+          category.map((cat, index) => (
+
+            <Link to={`/category/${cat.category_id}`} key={index}>
+            <div key={index} className='bg-gray-200 p-4 rounded-lg shadow-md'>
+              <h2 className='text-xl font-semibold'>{cat.category_name}</h2>
+            </div>
+
+            </Link>
+          ))
+        }
+      </div>
     </div>
   )
 }
